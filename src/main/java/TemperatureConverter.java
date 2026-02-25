2a1c4b1b598e3ae7a07cca9f16756eba8a49faeb
hyoneekim <suhyunkim@Suhyunui-MacBookAir.local>
2026-02-25 13:08:27 +0200

    code fixed

--- src/main/java/TemperatureConverter.java
+++ src/main/java/TemperatureConverter.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TemperatureConverter {
 
     public static double fahrenheitToCelsius(double f) {
@@ -14,7 +16,7 @@ public class TemperatureConverter {
     }
 
     public static void main(String[] args) {
-        java.util.Scanner input = new java.util.Scanner(System.in);
+        Scanner input = new java.util.Scanner(System.in);
 
         System.out.print("Enter a temperature: ");
         double a = input.nextDouble();
